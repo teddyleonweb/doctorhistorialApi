ba398dda0c2562b5403e43baa74f5779cbef693c
Teddy Leon <teddyleonweb@gmail.com>
2023-11-04 05:03:01 -0400

    Modificacion del modelo agregar historial id y apellido

--- models/pacientes.js
+++ models/pacientes.js
@@ -2,11 +2,12 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var PacienteSchema = new Schema({
   nombre: String, // Campo para el nombre del paciente
+  apellido:String,
   edad: Number, // Campo para la edad del paciente
   genero: String, // Campo para el género del paciente
   direccion: String, // Campo para la dirección del paciente
   ciudad: String, // Campo para la ciudad del paciente
-  pais: String // Campo para el país del paciente
-  // Agrega más campos según tus necesidades
+  pais: String, // Campo para el país del paciente
+ historial_id:String
 });
 module.exports = mongoose.model('Paciente', PacienteSchema);
